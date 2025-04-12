cb14b1d3c777cefbece09625754d664274592da4
1s22s1 <1s22s1@users.noreply.github.com>
2025-04-12 22:13:26 +0900

    bundle gem .

---
+++ spec/joke_api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe JokeApi do
+  it "has a version number" do
+    expect(JokeApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
